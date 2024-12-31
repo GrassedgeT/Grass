90eab22105d9f3ff79479283dffb5b0990e5db99
GrassedgeT <grassedge@outlook.com>
2024-12-31 17:48:40 +0800

    Add Readme

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,2 +1,18 @@
+mod build;
+
+use std::{env};
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum XtaskCmd {
+    build
+}
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: XtaskCmd
+}
+
 fn main() {
 }
