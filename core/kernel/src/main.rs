a48ac1765ddd1159269270bf05336372ade28adf
GrassedgeT <grassedge@outlook.com>
2025-01-03 21:09:35 +0800

    feat: support console output and shutdown

--- core/kernel/src/main.rs
+++ core/kernel/src/main.rs
@@ -1,7 +1,31 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
+mod console;
 mod lang_items;
+mod sbi;
+mod logger;
 
 use core::arch::global_asm;
+
 global_asm!(include_str!("boot/entry.asm"));
+#[unsafe(no_mangle)]
+pub fn rust_main() -> ! {
+    clear_bss();
+    logger::init();
+    println!("Hello, world!");
+    panic!("shutdown");
+}
+
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    unsafe {
+        (sbss as usize..ebss as usize).for_each(|a| {
+            (a as *mut u8).write_volatile(0);
+        });
+    }
+}
