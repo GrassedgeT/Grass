b06c24c1680c1d7181a60518355f6ae21e9edcab
GrassedgeT <grassedge@outlook.com>
2025-01-03 13:16:41 +0800

    change cargo build to cargo rustc

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -1,4 +1,4 @@
-use std::process;
+use std::{process, vec};
 use clap::Args;
 #[derive(Args, Debug)]
 pub struct BuildArgs {
@@ -9,8 +9,29 @@ pub struct BuildArgs {
 
 impl BuildArgs {
     pub fn build(&self) {
+        //common cargo args
+        let mut args = vec![
+            "rustc", "--package", "kernel",
+            "--target", "riscv64gc-unknown-none-elf",
+        ];
+
+        if self.release {
+            args.push("--release");
+        }
+
+        //rustc flags
+        let rustc_args = vec![
+            "--",
+            //use custom linker script
+            "-Clink-arg=-Tcore/kernel/src/linker.ld",
+            //force enable frame pointers
+            "-Cforce-frame-pointers=yes",
+        ];
+
+        args.extend(rustc_args);
+        
         process::Command::new("cargo")
-            .args(["build", "--package", "kernel"])
+            .args(args)
             .status()
             .expect("failed to build kernel");
     }
